063925ae401082fc4e67a651d139f41b06ac823e
expirales <lucas_fi@hotmail.com>
2025-02-13 14:27:53 -0300

    linter files

--- src/utils/cssClassNames.ts
+++ src/utils/cssClassNames.ts
@@ -1,26 +1,26 @@
-type ClassNamesObject = { [key: string]: boolean };
+type ClassNamesObject = { [key: string]: boolean }
 
 export function cssClassNames(
-    defaultClassName: string | ClassNamesObject,
-    otherClassNames?: ClassNamesObject
+  defaultClassName: string | ClassNamesObject,
+  otherClassNames?: ClassNamesObject
 ): string {
-    let classNames: string = '';
+  let classNames: string = ''
 
-    if (defaultClassName && typeof defaultClassName === 'string') {
-        classNames = defaultClassName;
-    }
+  if (defaultClassName && typeof defaultClassName === 'string') {
+    classNames = defaultClassName
+  }
 
-    if (defaultClassName && typeof defaultClassName === 'object') {
-        classNames = `${Object.keys(defaultClassName)
-            .filter((className) => defaultClassName[className] === true)
-            .join(' ')}`;
-    }
+  if (defaultClassName && typeof defaultClassName === 'object') {
+    classNames = `${Object.keys(defaultClassName)
+      .filter((className) => defaultClassName[className] === true)
+      .join(' ')}`
+  }
 
-    if (otherClassNames && typeof otherClassNames === 'object') {
-        classNames = `${classNames} ${Object.keys(otherClassNames)
-            .filter((className) => otherClassNames[className] === true)
-            .join(' ')}`;
-    }
+  if (otherClassNames && typeof otherClassNames === 'object') {
+    classNames = `${classNames} ${Object.keys(otherClassNames)
+      .filter((className) => otherClassNames[className] === true)
+      .join(' ')}`
+  }
 
-    return classNames.trim();
+  return classNames.trim()
 }
--- src/utils/randomId.ts
+++ src/utils/randomId.ts
@@ -1,5 +1,5 @@
 function randomId(): number {
-    return Math.floor(100000 + Math.random() * 900000);
+  return Math.floor(100000 + Math.random() * 900000)
 }
 
-export { randomId };
+export { randomId }
